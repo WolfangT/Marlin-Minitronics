bda35fb4f0e23c787c6cabe341d39208ffe6e778
Wolfang Torres <wolfang.torres@gmail.com>
2022-12-12 16:16:47 -0400

    upadate max feedrates

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1178,7 +1178,7 @@
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
 //#define DEFAULT_MAX_FEEDRATE          { 15, 15, 3, 3 }
-#define DEFAULT_MAX_FEEDRATE          { 50, 50, 3.5, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 50, 50, 3.5, 30 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
